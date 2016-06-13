5bb0585b295fe8dc727a58f16406e87e063b3bed
Bittu Joju <Bittu_Joju@comcast.com>
2016-06-13 12:53:34 -0700

    Changed omniauth file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,9 @@
+require 'rspotify/oauth'
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :spotify, "c04e750b614d43a9b8a982ecbbe6f062", "824da32005fc4624a29e46207e42e31c", scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+end
+
 require "oauth2"
 require "omniauth"
 require "securerandom"
